cd21ef7be8b1761be959612a64c5b25e048ee55c
DanielMorales1103 <dmorales@pacifiko.com>
2024-02-18 19:51:19 -0600

    implementación A-star

--- algorithms_search.py
+++ algorithms_search.py
@@ -1,12 +1,34 @@
 from collections import deque
+from queue import PriorityQueue
 
 # Clase Nodo donde guardara estado, padre, accion y el costo
 class Node():
-    def __init__(self, state, parent = None, action = None, path_cost = 0) -> None:
+    def __init__(self, state, parent = None, action = None, path_cost = 0, heuristic_cost=0) -> None:
         self.state = state
         self.parent = parent
         self.action = action
         self.path_cost = path_cost   
+        self.heuristic_cost = heuristic_cost
+    
+    def total_cost(self):
+        return self.path_cost + self.heuristic_cost
+    
+    def __lt__(self, other):
+        return self.total_cost() < other.total_cost()
+
+'''Justificación:
+La heurística de la distancia de Manhattan es apropiada para problemas donde las acciones posibles son limitadas
+a movimientos horizontales y verticales, como en un laberinto donde solo se permite avanzar en cuatro direcciones
+(arriba, abajo, izquierda y derecha). '''
+def heuristic_1(state, goal):
+    return abs(state[0] - goal[0]) + abs(state[1] - goal[1])
+'''Justificación 
+La heurística euclidiana es adecuada cuando se quiere considerar la "distancia recta"
+entre dos puntos en un espacio bidimensional.
+'''
+def heuristic_2(state, goal):
+    return ((state[0] - goal[0]) ** 2 + (state[1] - goal[1]) ** 2) ** 0.5
+
         
 # Breadth First Search
 def bfs(framework):    
@@ -73,4 +95,35 @@ def dfs(framework):
                 frontier.append(child)
         
     return None
-    
+
+def a_star(framework, heuristic_func):
+    start_node = Node(state=framework.initial, heuristic_cost=heuristic_func(framework.initial, framework.goal))
+    frontier = PriorityQueue()
+    frontier.put((start_node.total_cost(), start_node))
+
+    explored = set()
+
+    while not frontier.empty():
+        _, node = frontier.get()
+
+        if framework.is_goal(node.state):
+            actions, states = [], []
+            while node.parent is not None:
+                actions.append(node.action)
+                states.append(node.state)
+                node = node.parent
+            actions.reverse()
+            states.reverse()
+            return framework.result(actions, states)
+
+        explored.add(node.state)
+
+        for action, state in framework.actions(node.state):
+            child_path_cost = node.path_cost + framework.path_cost(node.path_cost, node.state, action, state)
+            child_heuristic_cost = heuristic_func(state, framework.goal)
+            child = Node(state=state, parent=node, action=action, path_cost=child_path_cost, heuristic_cost=child_heuristic_cost)
+
+            if state not in explored:
+                frontier.put((child.total_cost(), child))
+
+    return None
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import processing_image
-from algorithms_search import bfs, dfs 
+from algorithms_search import bfs, dfs, a_star,heuristic_1, heuristic_2
 from frameworks import Labyrinth
 
 def graph_search_lab(image_lab: str, size_square: int, algorithm):
@@ -14,12 +14,17 @@ def graph_search_lab(image_lab: str, size_square: int, algorithm):
     
     # Framework tipo Laberinto
     labyrinth = Labyrinth(matrix_discreted_image)
-    
+    heuristic_function1 = heuristic_1
+    heuristic_function2 = heuristic_2
     # Seleccionar algoritmo
     if algorithm == 'bfs':
         solution = bfs(labyrinth)
     elif algorithm == 'dfs':
         solution = dfs(labyrinth)
+    elif algorithm == 'a-star':
+        solution = a_star(labyrinth,heuristic_function1)
+    elif algorithm == 'a-star2':
+        solution = a_star(labyrinth,heuristic_function2)
     else:
         raise ValueError("Algoritmo no soportado")
     
@@ -33,5 +38,5 @@ if __name__ == "__main__":
     size_square = 25  # Tamaño del cuadrado para discretizar
     
     for image_path in images:
-        for algorithm in ['bfs', 'dfs']:  # Ejecutar ambos algoritmos
+        for algorithm in ['bfs', 'dfs','a-star','a-star2']:  # Ejecutar ambos algoritmos
             graph_search_lab(image_path, size_square, algorithm)
